9bf55140f5883033c745249b2681e702e2524402
樊亮 <1442585939@qq.com>
2018-08-14 18:25:34 +0800

    add redis function

--- controller/loginController.js
+++ controller/loginController.js
@@ -1,26 +1,38 @@
 
 const ApiError = require('../error/ApiError');
 const ApiErrorNames = require('../error/ApiErrorNames');
-
+const redis = require('redis');
 import database from '../utils/database';
+const client = redis.createClient();
+client.set('hello', {a:1, b:2}); // 注意，value会被转为字符串,所以存的时候要先把value 转为json字符串
 
 
+client.on('error',function (err) {
+   console.log('err message is:',err)
+});
 
 
+client.on('connect',runsample);
 
 
+function runsample() {
+    client.set('str','hello world', function (err, res) {
+        console.log('res data is:', res.toString())
+    });
+
+    client.get("str", function(err, reply) {
+        console.log('reply data is', reply.toString());
+    })
+}
 
 //获取用户
 
 exports.getUser = async (ctx, next) => {
 
-
-
-        let sql ='select * from user';
-
-    database.query(sql).then(function (r) {
-        console.log('ewde:', r)
-    })
+    //     let sql ='select * from user';
+    // database.query(sql).then(function (r) {
+    //     console.log('ewde:', r)
+    // });
 
 
 
@@ -29,6 +41,10 @@ exports.getUser = async (ctx, next) => {
     if(ctx.query.id != 1){
         throw new ApiError(ApiErrorNames.USER_NOT_EXIST);
     }else{
+        client.get('name', function(err, value){
+            console.log('get name is :',value)
+        });
+
         ctx.body = {
             username: 'fuck',
             age: 30,
